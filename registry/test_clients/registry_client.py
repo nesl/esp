6cd00483ac4be0aed21804789b40903c78d214f7
schmid <schmid@317b345d-ef17-0410-9c19-ab60824e0c6f>
2006-07-14 18:50:26 +0000

    commit changes to registry in order to be able to rename it

--- registry/test_clients/registry_client.py
+++ registry/test_clients/registry_client.py
@@ -8,20 +8,16 @@ def registerMediator():
 	pass
 
 def registerSystem():
-	xmldoc = minidom.parse("testsystem.xml")
+	register("registersystem.xml")
+
+def register(xml):
+	xmldoc = minidom.parse(xml)
+	server = SOAPpy.SOAPProxy("localhost:8080")
+	print server.register(xmldoc.toxml())
 
 if __name__ == '__main__':
-	xmlLocationQuery = minidom.parse("../xml/locationQuery1.xml")
-	xmldoc = minidom.parse("../xml/system1.xml")
-	server = SOAPpy.SOAPProxy("http://localhost:8080/")
-	print server.register(xmldoc.toxml())
 	
-	#print server.unRegister("<unregister></unregister>")
-	#print server.update("<update></update>")
-	print server.listSystems(xmlLocationQuery.toxml())
-
-	print server.unRegister(xmldoc.toxml())
-
+	registerSystem()
 
 	
 
